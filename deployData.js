9dab63916f45f77ab50cc409567f68331e5ff525
Rahma Rahadi <rahmalia.2002@gmail.com>
2023-05-22 21:55:29 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Rahmalia Rahadi'; // TODO: replace this
+
+const studentId = 'FE5653768'; // TODO: replace this
+
+const NetlifyDeployUrl = ''; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
